9eca50e885f6b9c5fe4217bb095c7d7acee1ace2
rvija <rvija@VIJAYSUSH>
2024-08-15 14:47:37 +0530

    Aishu

---
+++ src/demo/Hello.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Hello 
+{
+	
+
+	static void disp()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) 
+	{
+		disp();
+	}
+	
+
+
+}
